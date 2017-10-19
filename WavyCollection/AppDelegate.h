e9471e194b0533161b5420b8aea38a27af1a63a4
Sanjay Shah <Sanjay Shah>
2017-10-19 10:56:54 -0700

    Initial Commit

---
+++ WavyCollection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WavyCollection
+//
+//  Created by Sanjay Shah on 2017-10-19.
+//  Copyright © 2017 Sanjay Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WavyCollection/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WavyCollection
+//
+//  Created by Sanjay Shah on 2017-10-19.
+//  Copyright © 2017 Sanjay Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
